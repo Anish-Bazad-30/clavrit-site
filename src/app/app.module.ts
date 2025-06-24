02228269f1e7b55f660e5ef9082ba7817dcac2e7
Anish-Bazad <anishbazad@gmail.com>
2025-06-24 17:50:49 +0530

    Home page basic layout done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
