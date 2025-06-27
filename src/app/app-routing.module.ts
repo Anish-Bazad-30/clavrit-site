36aea5b1f6dde53773e1aab702698fbcddf71648
Anusha-Clavrit <anusha@clavrit.com>
2025-06-27 17:22:15 +0530

    added data to the home page & About page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {path:"" , component: LandingPageComponent},
-  {path:"about-us", component:AboutUsComponent}
+  {path:"about-us", component:AboutUsComponent},
+  {path:"services", component:ServicesComponent}
 ];
 
 @NgModule({
