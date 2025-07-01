2dfb48265fdd1c4972baf29dcb85bf09a5a70d33
Anusha-Clavrit <anusha@clavrit.com>
2025-07-01 11:53:53 +0530

    added services and contact us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServicesComponent } from './services/services.component';
 import { DetailViewComponent } from './detail-view/detail-view.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path:"" , component: LandingPageComponent},
   {path:"about-us", component:AboutUsComponent},
   {path:"services", component:ServicesComponent},
   {path:"detail-view", component:DetailViewComponent},
+  {path:"contact", component:ContactComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MainComponent } from './main/main.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { DetailViewComponent } from './detail-view/detail-view.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ServicesComponent } from './services/services.component';
     MainComponent,
     AboutUsComponent,
     DetailViewComponent,
-    ServicesComponent
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
